469fb2c0e26a79ceef95e0f50bd614b403b358e2
Even <evenkle@stud.ntnu.no>
2017-04-02 11:16:12 +0200

    Rough layout of the SaveData class

--- app/src/main/java/no/ntnu/prisonesc/SaveData.java
+++ app/src/main/java/no/ntnu/prisonesc/SaveData.java
@@ -1,8 +1,49 @@
 package no.ntnu.prisonesc;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
 /**
- * Created by even on 02.04.17.
+ * SaveData handle the saving of progress in the application
  */
 
 public class SaveData {
+
+    public static SaveData saveData;
+    private SharedPreferences data;
+
+    public SaveData(Context context){
+        data = PreferenceManager.getDefaultSharedPreferences(context);
+        try{
+            //load data from the preferences
+        } catch (ClassCastException e){
+            //cant load because it is the first time. Create from default
+        }
+    }
+
+
+    /**
+     * Get the current savedata from the users phone. Only one can be made.
+     * @param context android context
+     * @return instance of SaveData
+     */
+    public static SaveData getData(Context context){
+        if(saveData == null){
+            saveData = new SaveData(context);
+        }
+        return saveData;
+    }
+
+
+    /**
+     * Update savedata with updated data
+     */
+    private void updateData(){
+        SharedPreferences.Editor editor = data.edit();
+        //TODO add stuff to save;
+    }
+
+
+
 }
