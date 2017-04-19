c0f6ea1fe1c24b8fca6dcea3ea5dee1823b060e2
Simen Burud <sb@simen.codes>
2017-04-19 21:24:22 +0200

    Lots more images + jump from window animation ("welcome screen")

--- app/src/main/java/no/ntnu/prisonesc/MainActivity.java
+++ app/src/main/java/no/ntnu/prisonesc/MainActivity.java
@@ -1,13 +1,51 @@
 package no.ntnu.prisonesc;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView playerImage;
+    private int pixelSize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pixelSize = getResources().getDimensionPixelSize(R.dimen.world_scale_factor);
+
+        playerImage = (ImageView) findViewById(R.id.playerImage);
+        playerImage.setTranslationX(-20 * pixelSize);
+
+        playerImage.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+
+    public void startGame(View view) {
+        playerImage.animate()
+                .translationX(200 * pixelSize)
+                .rotation(20)
+                .setInterpolator(new AccelerateInterpolator())
+                .withEndAction(new Runnable() {
+                    @Override
+                    public void run() {
+                        startActivity(new Intent(MainActivity.this, GameActivity.class));
+                        finish();
+                    }
+                });
+    }
+
+    public void openShop(View view) {
+        startActivity(new Intent(this, ShopActivity.class));
+        finish();
     }
 }
