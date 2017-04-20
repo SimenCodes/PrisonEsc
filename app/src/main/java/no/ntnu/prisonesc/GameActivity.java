20aed1805be3fe16668881c9b1818aa77c02a61e
hflemmen <hflemmen@gmail.com>
2017-04-20 23:29:34 +0200

    Reduserte initiell gliderfactor.

--- app/src/main/java/no/ntnu/prisonesc/GameActivity.java
+++ app/src/main/java/no/ntnu/prisonesc/GameActivity.java
@@ -85,7 +85,7 @@ public class GameActivity extends AppCompatActivity implements Runnable, SensorE
 
         //Basevalues:
         double drag = 0.0028;
-        double gliderFactor = 0.15;
+        double gliderFactor = 0.05;
         int posY = 300;
         int velX = 5;
         int velY = 2;
